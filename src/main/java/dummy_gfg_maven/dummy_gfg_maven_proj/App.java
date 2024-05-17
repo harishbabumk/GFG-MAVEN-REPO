a249361dcd28d8d76a6b641e6b11f7facb1589c9
harishbabumk <harishbabumk@gmail.com>
2024-05-17 12:44:02 +0530

    First maven project

---
+++ src/main/java/dummy_gfg_maven/dummy_gfg_maven_proj/App.java
@@ -0,0 +1,13 @@
+package dummy_gfg_maven.dummy_gfg_maven_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
